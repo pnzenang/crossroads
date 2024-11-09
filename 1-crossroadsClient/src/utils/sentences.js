e2c3b1f0343f2dfbbeeb3958913bc03daffe1218
pnzenang <pnzenang@gmail.com>
2024-11-09 06:13:53 -0500

    11.09.3.commit

--- 1-crossroadsClient/src/utils/sentences.js
+++ 1-crossroadsClient/src/utils/sentences.js
@@ -6,7 +6,7 @@ let associations = [
     name: ` If your state has not pickup schedule, you can send us your package through the other shipping means (US Post office, UPS or FeDex) `,
   },
   {
-    name: `The best way to send packages through us is to create an account on our web application and start your package on your dashboard.`,
+    name: `The best way to send packages through Crossroads is to create an account on our web application and start your package on your dashboard.`,
   },
   {
     name: `Our phone number is (913)-999-5401 and our email is contact@crosslog.org, contact us to start discussing your package.`,
@@ -16,7 +16,7 @@ let associations = [
   },
 
   {
-    name: `We only pickup when we receive at least your pickup fee of $25.  our preferred mode of transaction is zelle, our phone number for zelle is (913)-999-5401.`,
+    name: `We only pickup when we receive at least your pickup fee of $25.  Our preferred mode of transaction is Zelle, our phone number for Zelle is (913)-999-5401.`,
   },
   {
     name: ` Please follow the schedule posted on our website, we may adjust the pickup time, but we should stay withing the days alloted for your region and your state.`,
