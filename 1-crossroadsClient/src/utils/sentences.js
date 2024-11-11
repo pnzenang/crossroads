0bcc51e4fb495cf6c2989c6fad98484c853593f1
pnzenang <pnzenang@gmail.com>
2024-11-11 06:59:24 -0500

    11.11.2.commit

--- 1-crossroadsClient/src/utils/sentences.js
+++ 1-crossroadsClient/src/utils/sentences.js
@@ -3,7 +3,7 @@ let associations = [
     name: `Choose your pickup date and time from the schedule posted for your state, we will honor it or call to adjust to a schedule to suit you and our driver. `,
   },
   {
-    name: ` If your state has not pickup schedule, you can send us your package through the other shipping means (US Post office, UPS or FeDex) `,
+    name: ` If your state has not pickup schedule, you can send us your package through the other shipping means (US Post office, UPS or FeDex). `,
   },
   {
     name: `The best way to send packages through Crossroads is to create an account on our web application and start your package on your dashboard.`,
@@ -12,7 +12,7 @@ let associations = [
     name: `Our phone number is (913)-999-5401 and our email is contact@crossshipping.org, contact us to start discussing your package.`,
   },
   {
-    name: `Take a picture of your package, send it by message with the package reference number to receive accurate shipping fee .`,
+    name: `Take a picture of your package, send it by message with the package reference number to receive accurate shipping fee.`,
   },
 
   {
